e5f6ad46c79fe1684fc47dafa91e591ebfb8da65
Jon <jarano93@gmail.com>
2021-01-27 14:10:52 -0800

    more pgp testing

--- test.js
+++ test.js
@@ -1,20 +1,49 @@
 const openpgp = require('openpgp');
 
-openpgp.generateKey({
-	userIds: [{ name: 'Foo'}],
-	curve: 'curve25519'
-})
-.then(({key, publicKeyArmored, privateKeyArmored, revocationCertificate }) => {
-	// openpgp.key.readArmored(publicKeyArmored)
-	openpgp.key.readArmored("foo")
-	.then(({ keys, err }) => {
-		console.log(keys);
-		if (err != null) {
-			console.log("FUG");
-		}
-		else {
-			console.log("NO ERRORS");
-		}
+// openpgp.generateKey({
+// 	userIds: [{ name: 'Foo'}],
+// 	curve: 'curve25519'
+// })
+// .then(({key, publicKeyArmored, privateKeyArmored, revocationCertificate }) => {
+// 	// openpgp.key.readArmored(publicKeyArmored)
+// 	openpgp.key.readArmored("foo")
+// 	.then(({ keys, err }) => {
+// 		console.log(keys);
+// 		if (err != null) {
+// 			console.log("FUG");
+// 		}
+// 		else {
+// 			console.log("NO ERRORS");
+// 		}
+// 	});
+// 	// .catch(console.log);
+// })
+
+async function main () {
+	const { key:k1 } = await openpgp.generateKey({
+		userIds: [{ name: 'Foo'}],
+		curve: 'curve25519'
 	});
-	// .catch(console.log);
-})
+	const { key:k2 } = await openpgp.generateKey({
+		userIds: [{ name: 'Bar'}],
+		curve: 'curve25519'
+	});
+	const message = openpgp.message.fromText("Hello, World!");
+	const encrypted = await openpgp.encrypt({
+		message: message,
+		// publicKeys: [k1.toPublic(), k2.toPublic()]
+		publicKeys: [k2.toPublic()]
+	})
+	console.log(encrypted.data);
+	const armoredMessage = await openpgp.message.readArmored(encrypted.data);
+	openpgp.decrypt({
+		message: armoredMessage,
+		privateKeys: k2
+	})
+	.then((msg) => { console.log(msg); })
+	.catch(console.err);
+
+	// console.log(decrypted);
+}
+
+main();
