ce09f4ca4fb9e1ef0553868f1c831754af1f860d
Jon <jarano93@gmail.com>
2021-01-28 21:25:23 -0800

    MORE TESTS -- DON'T USE THE GLOBAL FLAG

--- regex_test.js
+++ regex_test.js
@@ -1,4 +1,7 @@
 const text = "#### HANDSHAKE_0 1234 ####";
-const regex = /#### HANDSHAKE_0 (?<id>\d*) ####/g;
-const found = regex.exec(text);
+const regex = /#### HANDSHAKE_0 (?<id>\d*) ####/;
+console.log(text.match(regex));
+let found = regex.exec(text);
+console.log(found.groups.id);
+found = regex.exec(text);
 console.log(found.groups.id);
