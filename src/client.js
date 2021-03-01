b48fb67dc53d034e3040bb77fdc368f991baef34
Jon <jarano93@gmail.com>
2021-03-01 11:54:34 -0800

    more logging & throws errors

--- src/client.js
+++ src/client.js
@@ -1,5 +1,9 @@
 const input = require('./blessed-input.js');
 
+function badtoken (tokenPath) {
+	return Error(`Failed to login with token.  Check file "${tokenPath}" to make sure your token is correct & that you are connected to the internet`);
+}
+
 function init (tokenPath, fs, cli, Client,) {
 	const clientLogin = function (token) {
 		const stop = cli.spin("logging in with token");
@@ -12,34 +16,37 @@ function init (tokenPath, fs, cli, Client,) {
 			client.login(token)
 			.catch(err => {
 				stop();
-				badtoken(tokenPath);
-				reject(err);
+				reject(badtoken(tokenPath));
 			});
 		});
 	};
 	return new Promise((resolve, reject) => {
-		cli.log("checking for existing discord bot token...");
+		cli.log("\t- checking for existing discord bot token... ");
 		fs.access(tokenPath, fs.constants.R_OK, (err) => {
 			if (err == null) {
+				cli.cat("{green-fg}Found a token file{/}")
 				fs.readFile(tokenPath, 'utf8', (err, data) => {
 					if (err) {
 						reject(err);
 					}
 					else {
+						cli.log("\t- attempting to log in with existing... ");
 						clientLogin(data).then(resolve).catch(reject);
 						// clientLogin(data, tokenPath, Client).then(resolve).catch(reject);
 					}
 				});
 			}
 			else {
+				cli.cat("{yellow-fg}No token file found{/}")
+				cli.log("\t- Requesting token... ");
 				cli.question("Enter a discord bot token then press [ENTER] to continue", true)
 				.then(token => {
 					return clientLogin(token);
 					// return clientLogin(token, tokenPath, Client);
 				})
 				.then((client) => {
-					cli.log('{green-fg}Supplied token accepted!{/}');
-					console.log("\x1b[1m\x1b[32mSupplied token accepted!\x1b[0m");
+					// cli.log('{green-fg}Supplied token accepted!{/}');
+					// console.log("\x1b[1m\x1b[32mSupplied token accepted!\x1b[0m");
 					fs.writeFile(tokenPath, client.token, 'utf8', (err) => {
 						if (err) {
 							reject(err);
@@ -53,14 +60,6 @@ function init (tokenPath, fs, cli, Client,) {
 			}
 		});
 	})
-}
-
-// function clientLogin (token, tokenPath, Client) {
-	// console.log("attempting to login to discord with supplied token...");
-// }
-
-function badtoken (tokenPath) {
-	console.error(`\x1b[31mFailed to login with token.  Check file "${tokenPath}" to make sure your token is correct & that you are connected to the internet\x1b[0m`);
 }
 
 module.exports = init;
