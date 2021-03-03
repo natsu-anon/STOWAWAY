96b6fcb45252a80cb5a9a356e826ef8a4e04de86
Jon <jarano93@gmail.com>
2021-03-02 21:57:07 -0800

    learning about permissions

--- src/query.js
+++ src/query.js
@@ -1,88 +1,34 @@
 const fs = require('fs');
-const openpgp = require('openpgp');
-const process = require('process');
-const readline = require('readline');
 const { Client } = require('discord.js');
-
-// create an console interface
-const rl = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout
-});
+const API_TOKEN = './token DO NOT SHARE';
 
 
-function prepClient ({ database: db, key: key }) {
-	/* Client events to subscribe to:
-	*  N E C E S S A R Y
-	*    guildCreate -
-	*    message -
-	*
-	*  Nice to have
-	*    error - for debugging & error logging
-	*    invalidated
-	*    channelCreate
-	*    channelDelete
-	*    channelUpdate
-	*    guildBanAdd
-	*    guildBanRemove
-	*    guildDelete -
-	*    guildMemberAdd
-	*    guildMemberRemove
-	*    guildMemberUpdate
-	*    guildUnavailable
-	*    guildUpdate
-	*    messageUpdate
-	*    presenceUpdate
-	*    userUpdate
-	*/
-	return (client) => {
-		return client;
-	};
-}
-
-require('./database.js').Init(rl, openpgp)
-.then((data) => {
-	return new Promise((resolve, reject) => {
-		require('./client.js').Login(fs, Client, prepClient(data))
-		.then((client) => {
-			data.client = client
-			resolve(data);
-		})
-		.catch(reject);
+return new Promise((resolve, reject) => {
+	const client = new Client();
+	client.once('ready', () => {
+		resolve(client);
+	});
+	fs.readFile(API_TOKEN, 'utf8', (err, data) => {
+		if (err) {
+			reject(err);
+		}
+		else {
+			client.login(data);
+		}
 	});
 })
-.then(({key: key, database: db, client: client}) =>  {
+.then(client =>  {
 	console.log(`logged in as ${client.user.tag}\tid: ${client.user.id}`);
-	/*
-	foo = {}
 	client.guilds.cache.each((guild) => {
-		console.log(`[-] ${guild.name}`);
 		guild.channels.cache.filter(channel => channel.isText())
-		.each((channel) => {
-			if (channel.parent == null) {
-				console.log(`  > ${channel.name}`);
-			}
-			else {
-				if (channel.parentID in foo) {
-					foo[channel.parentID].push(channel.name);
-				}
-				else {
-					foo[channel.parentID] = [ channel.name ];
-				}
-			}
+		.each(channel => {
+			console.log(channel.name);
+			console.log(`\tcan view: ${channel.permissionsFor(client.user).has('VIEW_CHANNEL')}`);
+			console.log(`\tcan message: ${channel.permissionsFor(client.user).has('SEND_MESSAGES')}`);
+			console.log(`\tread history: ${channel.permissionsFor(client.user).has('READ_MESSAGE_HISTORY')}`);
 		});
-		let channel;
-		for (let id in foo) {
-			channel = guild.channels.cache.find(channel => channel.id == id)
-			console.log(`  [-] \x1b[32m${channel.name}\x1b[0m`);
-			for (let i = 0; i < foo[id].length; i++) {
-			// for (let foo in categories[id]) {
-				console.log(`    > ${foo[id][i]}`);
-			}
-		}
 	});
 	client.destroy()
 	process.exit(0)
-	*/
 })
 .catch((err) => { console.error(err); });
