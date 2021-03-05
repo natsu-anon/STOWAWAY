c450d8c538c678271ba5cc9a7fe3c129228c34a9
Jon <jarano93@gmail.com>
2021-03-05 12:33:35 -0800

    Update query.js

--- src/query.js
+++ src/query.js
@@ -1,6 +1,21 @@
 const fs = require('fs');
+const https = require('https');
 const { Client } = require('discord.js');
 const API_TOKEN = './token DO NOT SHARE';
+
+/* lmao didn't work
+// https.get("api.github.com/repos/natsu-anon/STOWAWAY/releases", response => {
+const options = {
+	// hostname: "api.github.com",
+	// path: "repos/octocat/hello-world/releases",
+	headers: { 'User-Agent': 'Mozilla/5.0' }
+};
+// https.get(options, response => {
+https.get("https://api.github.com/repos/octocat/hello-world/releases", options, response => {
+	response.on('data', data => { console.log(data); });
+	response.on('error', err => { throw err; });
+});
+*/
 
 
 return new Promise((resolve, reject) => {
