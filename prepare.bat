b01dfdb66e63b422fb8462ef1d94b4e5e08c0fb9
Jon <jarano93@gmail.com>
2021-03-05 10:25:16 -0800

    batch file to expedite my whole release pipeline a bit (doesn't work well with pkg tho rip)

---
+++ prepare.bat
@@ -0,0 +1,27 @@
+@ECHO OFF
+IF %1.==. GOTO No1
+MD releases\STOWAWAY_v%1_linux
+MD releases\STOWAWAY_v%1_macos
+MD releases\STOWAWAY_v%1_win
+MOVE releases\STOWAWAY-linux releases\STOWAWAY_v%1_linux\STOWAWAY
+MOVE releases\STOWAWAY-macos releases\STOWAWAY_v%1_macos\STOWAWAY
+MOVE releases\STOWAWAY-win.exe releases\STOWAWAY_v%1_win\STOWAWAY.exe
+COPY releases\about.txt releases\STOWAWAY_v%1_linux\about.txt
+COPY releases\about.txt releases\STOWAWAY_v%1_macos\about.txt
+COPY releases\about.txt releases\STOWAWAY_v%1_win\about.txt
+COPY releases\LICENSE.txt releases\STOWAWAY_v%1_linux\README.txt
+COPY releases\LICENSE.txt releases\STOWAWAY_v%1_macos\README.txt
+COPY releases\LICENSE.txt releases\STOWAWAY_v%1_win\README.txt
+COPY releases\README.txt releases\STOWAWAY_v%1_linux\LICENSE.txt
+COPY releases\README.txt releases\STOWAWAY_v%1_macos\LICENSE.txt
+COPY releases\README.txt releases\STOWAWAY_v%1_win\LICENSE.txt
+tar.exe -c -f releases\STOWAWAY_v%1_linux.zip releases\STOWAWAY_v%1_linux
+tar.exe -c -f releases\STOWAWAY_v%1_macos.zip releases\STOWAWAY_v%1_macos
+tar.exe -c -f releases\STOWAWAY_v%1_win.zip releases\STOWAWAY_v%1_win
+GOTO End1
+
+:No1
+	ECHO No version number passed
+GOTO End1
+
+:End1
