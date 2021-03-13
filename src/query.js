4ad75b474ac46ccb326ab376b740fee78dbd923a
Jon <jarano93@gmail.com>
2021-03-12 19:57:38 -0800

    Update query.js

--- src/query.js
+++ src/query.js
@@ -1,7 +1,10 @@
 const fs = require('fs');
 const https = require('https');
+const crypto = require('crypto');
+const Datastore = require('nedb');
 const { Client } = require('discord.js');
 const API_TOKEN = './token DO NOT SHARE';
+const REGEX = /^#{4} STOWAWAY #{4}$/m;
 
 /* lmao didn't work
 // https.get("api.github.com/repos/natsu-anon/STOWAWAY/releases", response => {
@@ -32,18 +35,30 @@ return new Promise((resolve, reject) => {
 		}
 	});
 })
-.then(client =>  {
+.then(client => {
 	console.log(`logged in as ${client.user.tag}\tid: ${client.user.id}`);
-	client.guilds.cache.each((guild) => {
-		guild.channels.cache.filter(channel => channel.isText())
-		.each(channel => {
-			console.log(channel.name);
-			console.log(`\tcan view: ${channel.permissionsFor(client.user).has('VIEW_CHANNEL')}`);
-			console.log(`\tcan message: ${channel.permissionsFor(client.user).has('SEND_MESSAGES')}`);
-			console.log(`\tread history: ${channel.permissionsFor(client.user).has('READ_MESSAGE_HISTORY')}`);
-		});
+	// client.on('message', message => {
+	// 	console.log(REGEX.test(message.content));
+	// });
+	const db = new Datastore({ filename: './stowaway.db', autoload: true });
+	db.persistence.setAutocompactionInterval(5000);
+	db.find({ user_key: { $exists: true } }, (err, docs) => {
+		if (err) {
+			throw err;
+		}
+		console.log(docs);
+		client.destroy();
+		process.exit(0);
 	});
-	client.destroy()
-	process.exit(0)
+	// client.guilds.cache.each(guild => {
+	// 	guild.channels.cache.filter(channel => channel.isText())
+	// 	.each(channel => {
+	// 		console.log(channel.name);
+	// 		console.log(`\tcan view: ${channel.permissionsFor(client.user).has('VIEW_CHANNEL')}`);
+	// 		console.log(`\tcan message: ${channel.permissionsFor(client.user).has('SEND_MESSAGES')}`);
+	// 		console.log(`\tread history: ${channel.permissionsFor(client.user).has('READ_MESSAGE_HISTORY')}`);
+	// 	});
+	// });
+	// console.log(crypto.getHashes());
 })
-.catch((err) => { console.error(err); });
+.catch(err => { console.error(err); });
