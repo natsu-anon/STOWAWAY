35b1a21ef417fc095cc45c55c977dc26632f745f
Jon <jarano93@gmail.com>
2021-03-18 10:56:02 -0700

    ty linter

--- src/list-channels.js
+++ src/list-channels.js
@@ -20,16 +20,16 @@ module.exports = (token, Client) => {
 			}
 		});
 	})
-	.then(client =>  {
+	.then(client => {
 		console.log(`logged in as ${client.user.tag}`);
-		let info = "In order for a server channel to be usable by STOWAWAY your bot must be able to:\n";
-		info += "  1. view the channel\n";
-		info += "  2. send messages to the channel\n";
-		info += "  3. read the message history of the channel\n";
-		info += "Available channels will appear in \x1b[42m\x1b[30mgreen\x1b[0m\n"
-		info += "Unavailable channels will appear in \x1b[31mred\x1b[0m along with what permissions your bot lacks";
+		let info = 'In order for a server channel to be usable by STOWAWAY your bot must be able to:\n';
+		info += '  1. view the channel\n';
+		info += '  2. send messages to the channel\n';
+		info += '  3. read the message history of the channel\n';
+		info += 'Available channels will appear in \x1b[42m\x1b[30mgreen\x1b[0m\n';
+		info += 'Unavailable channels will appear in \x1b[31mred\x1b[0m along with what permissions your bot lacks';
 		console.log(info);
-		client.guilds.cache.each((guild) => {
+		client.guilds.cache.each(guild => {
 			console.log(`\n\x1b[36m${guild.name}\x1b[0m`);
 			guild.channels.cache.filter(channel => channel.isText())
 			.each(channel => {
@@ -44,20 +44,20 @@ module.exports = (token, Client) => {
 					console.log(`  \x1b[31m#${channel.name}\x1b[0m`);
 				}
 				if (!viewFlag) {
-					log("- cannot view", 4);
+					log('- cannot view', 4);
 				}
 				if (!sendFlag) {
-					log("- cannot send messages", 4);
+					log('- cannot send messages', 4);
 				}
 				if (!historyFlag) {
-					log("- cannot read message history", 4);
+					log('- cannot read message history', 4);
 				}
 			});
 		});
-		client.destroy()
-		process.exit(0)
+		client.destroy();
+		process.exit(0);
 	})
-	.catch((err) => {
+	.catch(err => {
 		console.error(err);
 		process.exit(2);
 	});
