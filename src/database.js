484d768f2174fef9aded484675704bbf6a4c19fc
Jon <jarano93@gmail.com>
2021-03-19 11:01:47 -0700

    it's CLEAN

--- src/database.js
+++ src/database.js
@@ -1,11 +1,9 @@
 const Datastore = require('nedb');
 
 function init (dbFilename) {
-	return new Promise((resolve, reject) => {
-		const db = new Datastore({ filename: dbFilename, autoload: true });
-		db.persistence.setAutocompactionInterval(5000);
-		resolve(db);
-	});
+	const db = new Datastore({ filename: dbFilename, autoload: true });
+	db.persistence.setAutocompactionInterval(5000);
+	return Promise.resolve(db);
 }
 
 module.exports = init;
