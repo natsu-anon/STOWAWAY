fbbcf0c25b63978411d8f77c87fac67c2aa4c923
Jon <jarano93@gmail.com>
2021-03-22 12:22:04 -0700

    More Robust MessagesModel

--- src/models/messages-model.js
+++ src/models/messages-model.js
@@ -1,102 +1,197 @@
 const Model = require('./model.js');
 
-class Handshake {
-	constructor (date, author) {
+class ChannelMessage {
+	constructor (date, member, verified, signed, plainText) {
 		this.date = date;
-		this.author = author;
+		if (member.nickname != null) {
+			this.name = signed ? member.nickname : `{green-fg}${member.nickname}{/green-fg}`;
+		}
+		else {
+			this.name = signed ? member.user.username : `{green-fg}${member.user.username}{/green-fg}`;
+		}
+		this.verified = verified;
+		this.plainText = plainText;
 	}
 
 	get text () {
-		return `\t{green-bg}{black-fg}${this.author.tag} handshake at ${this.date.toLocaleDateString()} ${this.date.toLocaleTimeString()}{/}`;
-	}
-}
-
-class Message {
-	constructor (date, author, content) {
-		this.date = date;
-		this.author = author;
-		this.content = content;
-	}
-
-	get text () {
-		return `{blue-fg}[${this.date.toLocaleDateString()} ${this.date.toLocaleTimeString()}] <{/}${this.author.username}{blue-fg}>{/} ${this.content}{/}`;
+		if (this.verified) {
+			return `{blue-fg}[${this.date.toLocaleDateString()} ${this.date.toLocaleTimeString()}] <{/}${this.name}{blue-fg}>{/} ${this.plainText}{/}`;
+		}
+		else {
+			return `{yellow-fg}[${this.date.toLocaleDateString()} ${this.date.toLocaleTimeString()}] <{/}${this.name}{yellow-fg}>{/} ${this.plainText}{/}`;
+		}
 	}
 }
 
 class DecryptionFailure {
-	constructor (date, author) {
+	constructor (date, member) {
 		this.date = date;
-		this.author = author;
+		this.name = member.nickname != null ? member.nickname : member.user.username;
+		this.tag = member.user.tag;
 	}
 
 	get text () {
-		return `{red-fg}[${this.date.toLocaleDateString()} ${this.date.toLocaleTimeString()}] <{underline}${this.author.username}{/underline}> Failed to decrypt message from {underline}${this.author.tag}{/}`;
+		return `{red-fg}[${this.date.toLocaleDateString()} ${this.date.toLocaleTimeString()}] <{underline}${this.name}{/underline}> Failed to decrypt message from {underline}${this.tag}{/} consider repeating the handshake protocol.  Press 'Ctrl-H' for help.`;
+	}
+}
+
+class Handshake {
+	constructor (date, member, goodFlag) {
+		this.date = date;
+		this.name = member.nickaname != null ? member.nickname : member.user.username;
+		this.tag = member.user.tag;
+		this.goodFlag = goodFlag;
+	}
+
+	get text () {
+		if (this.goodFlag) {
+			return `\t{green-bg}{black-fg}$this.name (${this.author.tag}) handshake at ${this.date.toLocaleDateString()} ${this.date.toLocaleTimeString()}{/}`;
+		}
+		else {
+			return `\t{red-bg}{black-fg}$this.name ${this.author.tag} bad handshake at ${this.date.toLocaleDateString()} ${this.date.toLocaleTimeString()}{/}`;
+		}
+	}
+}
+
+class SignedKey {
+	constructor (date, member) {
+		this.date = date;
+		this.name = member.nickname != null ? member.nickname : member.user.username;
+		this.tag = member.user.tag;
+	}
+
+	get text () {
+		return `\t{cyan-bg}{black-fg}${this.name} (${this.author.tag}) signed your key at ${this.date.toLocaleDateString()} ${this.date.toLocaleTimesString()}{/}`;
+	}
+}
+
+class KeyUpdate {
+	constructor (date, member) {
+		this.date = date;
+		this.name = member.nickname != null ? member.nickname : member.user.username;
+		this.tag = member.user.tag;
+	}
+
+	get text () {
+		return `\t{cyan-bg}{black-fg}${this.name} (${this.author.tag}) updated his key at ${this.date.toLocaleDateString()} ${this.date.toLocaleTimesString()}{/}`;
+	}
+}
+
+class Revocation {
+	constructor (date, member, blockReason) {
+		this.date = date;
+		this.name = member.nickname != null ? member.nickname : member.user.username;
+		this.tag = member.user.tag;
+		this.blockReason = blockReason;
+	}
+
+	get text () {
+		if (this.blockReason != null) {
+			let res = `\t{red-bg}{underline}BLOCKED {black-fg}$this.name ${this.author.tag} from revoking his key at ${this.date.toLocaleDateString()} ${this.date.toLocaleTimeString()}{/}\n`;
+			res += `{red-fg}{underline}REASON{/underline}: ${this.blockReason}{/}`;
+			return res;
+
+		}
+		else {
+			return `\t{green-bg}{black-fg}${this.name} (${this.author.tag}) revoked his key at ${this.date.toLocaleDateString()} ${this.date.toLocaleTimesString()}{/}`;
+		}
 	}
 }
 
 
 class MessagesModel extends Model {
-	#content;
+	#messages;
 
-	constructor () {
+	constructor (stowaway) {
 		super();
-		this.#content = [];
+		stowaway.on('channel message', this.#channelMessage);
+		stowaway.on('decryption failure', this.#decryptionFailure);
+		stowaway.on('handshake', this.#handshake);
+		stowaway.on('signed key', this.#signedKey);
+		stowaway.on('key update', this.#keyUpdate);
+		stowaway.on('revocation', this.#keyUpdate);
 	}
 
 	listen (channelId) {
-		this.#content = [];
-		this.message = (ts, date, author, content) => {
-			// console.log(`receive(): ${author}`);
-			if (this.content[ts] === undefined) {
-				this.#content.push({
-					timestamp: ts,
-					item:  new Message(date, author, content)
-				});
-				this.emit('update', res.text);
-			}
-		};
-		this.decryptionFailure = (ts, date, author) => {
-			if (this.content[ts] === undefined) {
-				const res = new DecryptionFailure(date, author);
-				this.content[ts] = res;
-				this.emit('update', res.text);
-			}
-		};
-		this.handshake = (ts, date, author) => {
-			if (this.content[ts] === undefined) {
-				const res = new Handshake(date, author);
-				this.content[ts] = res;
-				this.emit('update', res.text);
-			}
-		};
+		this.#messages = [];
+		this.channelId = channelId;
 	}
 
-	timestamp (ts, id) {
-		if (this.ts0 == null || ts < this.ts0) {
-			this.ts0 = ts;
-			this._oldest = id;
-		}
-		if (this.ts1 == null || ts > this.ts1) {
-			this.ts1 = ts;
-			this._newest = id;
-		}
+	get oldestTs () {
+		return this.#messages[0].timestamp;
 	}
 
-	get oldest () {
-		return this._oldest;
-	}
-
-	get newest () {
-		return this._newest;
+	get newestTs () {
+		return this.#messages[this.#messages.length - 1].timestamp;
 	}
 
 	get text () {
-		const ts = Object.keys(this.content).sort((ts0, ts1) => ts0 - ts1);
-		const res = [];
-		for (let i = 0; i < ts.length; i++) {
-			res.push(this.content[ts[i]].text);
+		return this.#messages.map(message => message.content.text).join('\n');
+	}
+
+	#channelMessage (message, data) {
+		if (message.channel.id === this.channelId) {
+			this.#messages.push({
+				timestamp: message.createdTimestamp,
+				content: new ChannelMessage(message.createdAt, message.member, data.verified, data.signed, data.plainText)
+			});
+			this.#sortThenUpdate();
 		}
-		return res.join('\n');
+	}
+
+	#decryptionFailure (message) {
+		if (message.channel.id === this.channelId) {
+			this.#messages.push({
+				timestamp: message.createdTimestamp,
+				content: new DecryptionFailure(message.createdAt, message.member)
+			});
+			this.#sortThenUpdate();
+		}
+	}
+
+	#handshake (message, goodFlag) {
+		if (message.channel.id === this.channelId) {
+			this.#messages.push({
+				timestamp: message.createdTimestamp,
+				content: new Handshake(message.createdAt, message.member, goodFlag)
+			});
+			this.#sortThenUpdate();
+		}
+	}
+
+	#signedKey (message) {
+		if (message.channel.id === this.channelId) {
+			this.#messages.push({
+				timestamp: message.createdTimestamp,
+				content: new SignedKey(message.createdAt, message.member)
+			});
+			this.#sortThenUpdate();
+		}
+	}
+
+	#keyUpdate (message) {
+		if (message.channel.id === this.channelId) {
+			this.#messages.push({
+				timestamp: message.createdTimestamp,
+				content: new KeyUpdate(message.createdAt, message.member)
+			});
+			this.#sortThenUpdate();
+		}
+	}
+
+	#revocation (message, blockReason) {
+		if (message.channel.id === this.channelId) {
+			this.#messages.push({
+				timestamp: message.createdTimestamp,
+				content: new Revocation(message.createdAt, message.member, blockReason)
+			});
+			this.#sortThenUpdate();
+		}
+	}
+
+	#sortThenUpdate() {
+		this.#messages.sort((a, b) => { return a.timestamp - b.timestamp; });
+		this.emit('update');
 	}
 }
 
