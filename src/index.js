9a31d31870b6a4a13aa9b8592e2d1bcb978fd25f
Jon <jarano93@gmail.com>
2021-03-29 11:43:35 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
-const process = require('process');
-const { Client } = require('discord.js');
-
 const SAVE_DIR = './DO_NOT_SHARE/';
 const DATABASE = SAVE_DIR + 'stowaway.db';
 const API_TOKEN = SAVE_DIR + 'stowaway.token';
 const PRIVATE_KEY = SAVE_DIR + 'stowaway.key';
-const REVOCATION_CERTIFICATE = SAVE_DIR + 'stowaway.revoke';
+const REVOCATION_CERTIFICATE = 'stowaway.revoke';
 
 const VERSION = '1.0.0';
 const BANNER = `      _  __ __        __        __
@@ -15,7 +12,7 @@ This software is licensed under the WTFPL`;
 
 
 if (process.argv.length > 2 && (process.argv[2] === '--channels' || process.argv[2] === '-c')) {
-	require('./list-channels.js')(API_TOKEN, Client);
+	require('./list-channels.js')(API_TOKEN);
 }
 else if (process.argv.length > 2 && (process.argv[2] === '--version' || process.argv[2] === '-v')) {
 	console.log(VERSION);
@@ -26,6 +23,9 @@ else if (process.argv.length > 2 && (process.argv[2] === '--about' || process.ar
 else if (process.argv.length > 2 && (process.argv[2] === '--help' || process.argv[2] === '-h')) {
 	require('./help.js')();
 }
+else if (process.argv.length > 2 && process.arg[2] === '--revoke') {
+	console.log('todo');
+}
 else {
-	require('./main.js')(BANNER, DATABASE, API_TOKEN, PRIVATE_KEY, REVOCATION_CERTIFICATE);
+	require('./main.js')(VERSION, BANNER, DATABASE, API_TOKEN, PRIVATE_KEY, REVOCATION_CERTIFICATE);
 }
