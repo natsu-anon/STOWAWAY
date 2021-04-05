f89bf5faf4ee2f26dd0ba7bafa155d3caed1fdee
Jon <jarano93@gmail.com>
2021-04-05 11:32:04 -0700

    Update about.js

--- src/about.js
+++ src/about.js
@@ -1,6 +1,6 @@
 module.exports = function(banner) {
-	console.log(banner);
-	let about = '\nSTOWAWAY is an End-to-End PGP encryption layer for Discord focused on making privacy easy & accessible via bots.\n';
+	let about = banner;
+	about += '\n\nSTOWAWAY is an End-to-End PGP encryption layer for Discord focused on making privacy easy & accessible via bots.\n';
 	about += 'Layed out plainly:\n';
 	about += '  1. Your computer encrypts your message\n';
 	about += '  2. Your bot sends the encrypted message over the network\n';
@@ -12,7 +12,7 @@ module.exports = function(banner) {
 	about += 'The official github repo is: \x1b[4mhttps://github.com/natsu-anon/STOWAWAY\x1b[0m\n';
 	about += 'That page includes instructions on how to setup a Discord bot & add it to servers with pictures & step-by-step instructions.\n\n';
 	about += 'To learn more about the WTFPL that STOWAWAY is licensed under see: \x1b[4mhttp://www.wtfpl.net/\x1b[0m\n\n';
-	about += 'Thanks for downloading & I hope you find this software useful';
+	about += 'Thanks for downloading & I hope you find this software useful\n';
 	about += '<3';
-	console.log(about);
+	return about;
 };
