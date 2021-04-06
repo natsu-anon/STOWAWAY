59369d9c5c0c77ab241433e1c0eed9c312d0b52f
Jon <jarano93@gmail.com>
2021-04-05 18:06:00 -0700

    tree struct for servers w/ channels

---
+++ src/models/server-channel-struct.js
@@ -0,0 +1,13 @@
+// TODO: write this then use in everything else
+class ServerChannel {
+
+	// add/remove/update server
+
+	// add/remove/update channel
+
+	// flatten
+
+	// sort func
+}
+
+module.exports = ServerChannel;
