523497a2a3567f8aeabe0210949356197003609b
Jon <jarano93@gmail.com>
2021-04-08 12:15:32 -0700

    W O R K S

--- src/mediators/channel-navigator.js
+++ src/mediators/channel-navigator.js
@@ -1,45 +1,62 @@
 class ChannelNavigator {
-	#model;
 	#index;
+	#position;
 
-	constructor (model, initIndex) {
-		this.#model = model;
-		this.#index = initIndex();
+	constructor (struct, initIndex) {
+		this.struct = struct;
+		this.#index = initIndex(this);
 	}
 
 	get index () {
 		return this.#index;
 	}
 
+	get percentage () {
+		if (this.struct.data.length === 0) {
+			return 0;
+		}
+		else {
+			return this.#index / (this.struct.data.length + this.struct.numChannels());
+		}
+	}
+
 	set index (value) {
 		this.#index = value;
 	}
 
-	get #data () {
-		return this.#model.data;
+	get data () {
+		return this.struct.data;
 	}
 
 	get channel () {
 		if (this.#index != null) {
-			return this.#data[this.#index];
+			return this.data[this.#position.server].channels[this.#position.channel];
 		}
 		else {
 			return null;
 		}
 	}
 
+	setPosition (server, channel) {
+		this.#position = { server, channel };
+	}
+
 	find (channelId) {
-		const i = this.#data.findIndex(({ id }) => {
-			return id === channelId;
-		});
-		if (i >= 0) {
-			this.#index = i;
-			return true;
+		let index = 0;
+		for (let i = 0; i < this.data.length; i++) {
+			index++;
+			for (let j = 0; j < this.data[i].channels.length; j++) {
+				if (this.data[i].channels[j].id === channelId) {
+					this.#index = index;
+					this.setPosition(i, j);
+					return true;
+				}
+				else {
+					index++;
+				}
+			}
 		}
-		else {
-			this.#index = null;
-			return false;
-		}
+		return false;
 	}
 
 	scrollChannels (nextFlag) {
@@ -51,9 +68,24 @@ class ChannelNavigator {
 	}
 
 	#nextChannel () {
-		if (this.#data.length > 0) {
-			this.#index = ++this.#index % this.#data.length;
-			return true;
+		if (this.struct.numChannels() > 0) {
+			if (this.#position.channel + 1 < this.data[this.#position.server].channels.length) {
+				this.#index++;
+				this.#position.channel++;
+				return true;
+			}
+			else {
+				let temp = 1;
+				for (let i = this.#position.server + 1; i < this.data.length; i++) {
+					temp++;
+					if (this.data[i].channels.length > 0) {
+						this.#index += temp;
+						this.setPosition(i, 0);
+						return true;
+					}
+				}
+				return this.#firstChannel();
+			}
 		}
 		else {
 			return false;
@@ -61,9 +93,35 @@ class ChannelNavigator {
 	}
 
 	#prevChannel () {
-		if (this.#data.length > 0) {
-			this.#index = --this.#index > -1 ? this.#index : this.#data.length - 1;
-			return true;
+		if (this.struct.numChannels() > 0) {
+			if (this.#position.channel > 0) {
+				this.#index--;
+				this.#position.channel--;
+				return true;
+			}
+			else {
+				let temp = 1;
+				for (let i = this.#position.server - 1; i >= 0; i--) {
+					temp++;
+					if (this.data[i].channels.length > 0) {
+						this.#index -= temp;
+						this.setPosition(i, this.data[i].channels.length - 1);
+						return true;
+					}
+				}
+				temp = 0;
+				for (let i = this.data.length - 1; i >= 0; i--) {
+					if (this.data[i].channels.length > 0) {
+						for (let j = 0; j < i; j++) {
+							temp += this.data[j].channels.length + 1;
+						}
+						this.#index = temp + this.data[i].channels.length;
+						this.setPosition(i, this.data[i].channels.length - 1);
+						return true;
+					}
+				}
+				return false;
+			}
 		}
 		else {
 			return false;
@@ -71,55 +129,77 @@ class ChannelNavigator {
 	}
 
 	#nextServer () {
-		if (this.#data.length > 0) {
-			if (this.#index === this.#data.length - 1) {
-				this.#index = 0;
+		if (this.struct.numChannels() > 0) {
+			let temp = this.data[this.#position.server].channels.length - this.#position.channel + 1;
+			for (let i = this.#position.server + 1; i < this.data.length; i++) {
+				if (this.data[i].channels.length > 0) {
+					this.setPosition(i, 0);
+					this.#index += temp;
+					return true;
+				}
+				else {
+					temp++;
+				}
 			}
-			else if (this.#index != null) {
-				this.#index = this.#data.findIndex(({ serverId }) => {
-					return serverId !== this.#data[this.#index].serverId;
-				}, this.#index);
-				this.#index = this.#index > -1 ? this.#index : 0;
-			}
-			else {
-				this.#index = 0;
-			}
-			return true;
+			return this.#firstChannel();
 		}
 		else {
 			return false;
 		}
 	}
 
+	// actually takes you to the 1st channel in server if not already at 1st
 	#prevServer () {
-		if (this.#data.length > 0) {
-			if (this.#index === 0) {
-				this.#index = this.#data.findIndex(({ serverId }) => {
-					return serverId === this.#data[this.#data.length - 1].serverId;
-				});
+		if (this.struct.numChannels() > 0) {
+			if (this.#position.channel > 0) {
+				this.#index -= this.#position.channel;
+				this.#position.channel = 0;
+				return true;
 			}
-			else if (this.#index != null) {
-				const serverId = this.#data[this.#index].serverId;
-				for (let i = this.#index - 1; i > -1; i--) {
-					if (this.#data[i].serverId !== serverId) {
-						this.#index = this.#data.findIndex(({ serverId }) => {
-							return serverId === this.#data[i].serverId;
-						});
+			else {
+				let temp = 1;
+				for (let i = this.#position.server - 1; i >= 0; i--) {
+					if (this.data[i].channels.length > 0) {
+						this.#index -= temp + this.data[i].channels.length;
+						this.setPosition(i, 0);
+						return true;
+					}
+					else {
+						temp++;
+					}
+				}
+				temp = 1;
+				for (let i = this.data.length - 1; i >= 0; i--) {
+					if (this.data[i].channels.length > 0) {
+						for (let j = 0; j < i; j++) {
+							temp += this.data[j].channels.length + 1;
+						}
+						this.#index = temp;
+						this.setPosition(i, 0);
 						return true;
 					}
 				}
-				this.#index = this.#data.findIndex(({ serverId }) => {
-					return serverId === this.#data[this.#data.length - 1].serverId;
-				});
+				return false;
 			}
-			else {
-				this.#index = 0;
-			}
-			return true;
 		}
 		else {
 			return false;
 		}
+	}
+
+	#firstChannel () {
+		let temp = 1;
+		for (let i = 0; this.data.length; i++) {
+			if (this.data[i].channels.length > 0) {
+				this.#index = temp;
+				this.setPosition(i, 0);
+				return true;
+			}
+			else {
+				temp++;
+			}
+		}
+		return false;
 	}
 }
 
