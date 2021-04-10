4d1354475b5f33f5b54ab5196ac1468db93098ff
Jon <jarano93@gmail.com>
2021-04-10 15:40:43 -0700

    additional testing

--- blessed-test.js
+++ blessed-test.js
@@ -1,3 +1,7 @@
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - *
+/* NOTE: IMPOSSIBLE TO DISCENERN SHIFT+ENTER FROM ENTER :^(
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
+
 const blessed = require('blessed');
 
 const screen = blessed.screen({
@@ -9,6 +13,7 @@ const screen = blessed.screen({
 	fullunicode: true, // allows for meme double-wide characters
 });
 
+/*
 const box = blessed.box({
 	parent: screen,
 	top: 0,
@@ -19,8 +24,31 @@ const box = blessed.box({
 });
 
 screen.on('keypress', (char, key) => {
-	box.setContent(`character: ${char}, full: ${key.full}`);
+
+	let temp = `character: ${char}`;
+	for (const val in key) {
+		temp += `\nkey.${val}: ${key[val]}`;
+	}
+	// box.setContent(`character: ${char}, full: ${key.full}`);
+	box.setContent(temp);
 	screen.render();
 });
+*/
+const input = blessed.textbox({
+	parent: screen,
+	top: 0,
+	left: 0,
+	width: '100%',
+	height: '100%',
+	inputOnFocus: true
+});
+input.focus();
+
+
 screen.onceKey('C-c', () => { process.exit(0); });
+input.on('submit', () => {
+	input.clearValue();
+	screen.render();
+});
+input.onceKey('C-c', () => { process.exit(0); });
 screen.render();
