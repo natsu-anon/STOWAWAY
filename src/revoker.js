8862d1db771297ab7a8ef9e415dc691f0230bccf
Jon <jarano93@gmail.com>
2021-04-10 15:41:32 -0700

    noice little class that handles the revocation process (use this for the command line)

---
+++ src/revoker.js
@@ -0,0 +1,68 @@
+const fs = require('fs');
+const openpgp = require('openpgp');
+
+function writeFile (file, data, encoding='utf8') {
+	return new Promise((resolve, reject) => {
+		fs.writeFile(file, data, encoding, err => {
+			if (err != null) {
+				reject(err);
+			}
+			else {
+				resolve();
+			}
+		});
+	});
+}
+
+class Revoker {
+	constructor (stowaway, client, keyPath, revocationPath) {
+		this.stowaway = stowaway;
+		this.client = client;
+		this.keyPath = keyPath;
+		this.revocationPath = revocationPath;
+	}
+
+	// assume it has the revocation certificate
+	checkChallenge (input) {
+		return input.toUpperCase() === this.challenge;
+	}
+
+	setKey (key) {
+		this.key = key;
+		return this;
+	}
+
+	async setNickname (promise) {
+		this.nickname = await promise;
+		return this;
+	}
+
+	async setPassphrase (promise) {
+		this.passphrase = await promise;
+		return this;
+	}
+
+	async setRevocationCertificate (promise) {
+		this.revocationCertificate = await promise;
+		return this;
+	}
+
+	async revoke () {
+		if (this.passphrase != null && this.nickname != null && this.key != null && this.revocationCertificate != null) {
+			const { key, revocationCertificate } = await openpgp.generateKey({
+				type: 'ecc',
+				curve: 'curve25519',
+				passphrase: this.passphrase,
+				userIds: [{
+					name: this.nickname,
+				}]
+			});
+			this.stowaway.revokeKey(this.client, this.key, key, this.revocationCertificate);
+			this.key = key;
+			await writeFile(this.revocationPath, revocationCertificate);
+			await writeFile(this.keyPath, key);
+		}
+	}
+}
+
+module.exports = Revoker;
