3db5bd14fca5199a75faecd8a6ff0a8eab460a25
Jon <jarano93@gmail.com>
2021-04-16 11:43:58 -0700

    Create test-data.js

---
+++ test-data.js
@@ -0,0 +1,10 @@
+module.exports = {
+	token0: '_test0.token',
+	token1: '_test1.token',
+	key0: '_test0.key',
+	key1: '_test1.key',
+	db0: '_test0.db',
+	db1: '_test1.db',
+	channels0: [],
+	channels1: [],
+};
