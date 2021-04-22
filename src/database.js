2f700125cedccf3cd165292509fac8b6bc87702a
Jon <jarano93@gmail.com>
2021-04-22 12:13:45 -0700

    why use promises so much?

--- src/database.js
+++ src/database.js
@@ -3,7 +3,7 @@ const Datastore = require('nedb');
 function init (dbFilename) {
 	const db = new Datastore({ filename: dbFilename, autoload: true });
 	db.persistence.setAutocompactionInterval(5000);
-	return Promise.resolve(db);
+	return db;
 }
 
 module.exports = init;
