a972aa5f977aacad5e69ae8c1bdf18a8d12ed9fa
Jon <jarano93@gmail.com>
2021-05-18 11:00:39 -0700

    use lokijs

--- deadhead.js
+++ deadhead.js
@@ -1,12 +1,12 @@
 const process = require('process');
 const { Stowaway } = require('./src/stowaway.js');
-const Datastore = require('nedb');
+const database = require('./src/database.js');
 const { clientLogin, genKey, loadKey } = require('./test.js');
 const { token1, keyPath1, db1, channels1 } = require('./test-data.js');
 
 async function launch (freshFlag) {
-	const VERSION = '1.0.0-daemon';
-	const db = freshFlag ? new Datastore() : new Datastore({ filename: db1, autoload: true });
+	const VERSION = '1.1.0-deadhead';
+	const db = freshflag ? await database('temp.db', false) : await database(db1);
 	db.persistence.setAutocompactionInterval(10000);
 	const client = await clientLogin(token1);
 	const { key } = freshFlag ? await genKey() : await loadKey(keyPath1);
