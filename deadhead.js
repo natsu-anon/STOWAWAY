93ea0543c1aa2ce8a32ebd192728452b40430f0c
Jon <jarano93@gmail.com>
2021-05-26 11:29:15 -0700

    got deadhead working with lokijs

--- deadhead.js
+++ deadhead.js
@@ -5,11 +5,12 @@ const { clientLogin, genKey, loadKey } = require('./test.js');
 const { token1, keyPath1, db1, channels1 } = require('./test-data.js');
 
 async function launch (freshFlag) {
+	console.log(`### DEADHEAD ${freshFlag ? 'FRESH' : 'CACHED'}`);
 	const VERSION = '1.1.0-deadhead';
-	const db = freshFlag ? await database('temp.db', false) : await database(db1);
+	const { channels, peers, revocations } = freshFlag ? await database('temp.db', false) : await database(db1);
 	const client = await clientLogin(token1);
 	const { key } = freshFlag ? await genKey() : await loadKey(keyPath1);
-	const stowaway = new Stowaway(db, keyPath1, VERSION);
+	const stowaway = new Stowaway(channels, peers, revocations, keyPath1, VERSION, 'deadhead', true);
 	stowaway.on('notify', (color, text) => {
 		console.log(`\t${client.user.tag} ${color}: ${text}`);
 	});
@@ -18,7 +19,7 @@ async function launch (freshFlag) {
 	});
 	stowaway.launch(client, key);
 	let channel;
-	console.log(`${freshFlag ? 'fresh' : 'cached'} deadhead: ${client.user.tag}`);
+	console.log(`deadhead: ${client.user.tag}`);
 	for (let i = 0; i < channels1.length; i++) {
 		channel = await stowaway.loadChannel(await client.channels.fetch(channels1[i]));
 		console.log(`\tdeadheading: ${channel.guild.name} #${channel.name}`);
