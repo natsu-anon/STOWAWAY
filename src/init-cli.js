3b5fa8592cc5f666c3882506c6adaf406e2767e7
Jon <jarano93@gmail.com>
2021-06-01 17:10:41 -0700

    ignore escape & ctrl-sequences

--- src/init-cli.js
+++ src/init-cli.js
@@ -41,7 +41,7 @@ class BlessedInit {
 			tabSize: 2,
 			dockborders:true,
 			fullunicode: true,
-			ignoreLocked : [ 'C-c' ],
+			ignoreLocked : [ 'C-c', 'C-r', 'C-k', 'C-a', 'escape' ],
 		});
 		screen.title = title;
 		this.content = [ banner ];
