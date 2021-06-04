4b00468e16db50f18b0baa07b63de2bde670b1e7
Jon <jarano93@gmail.com>
2021-06-03 17:24:22 -0700

    made the load callback it's own function and added bruh()

--- src/database.js
+++ src/database.js
@@ -1,5 +1,25 @@
 const loki = require('lokijs');
 
+function addCollections (db) {
+	let channels = db.getCollection('channels');
+	if (channels == null) {
+		channels = db.addCollection('channels');
+	}
+	let peers = db.getCollection('peers');
+	if (peers == null) {
+		peers = db.addCollection('peers');
+	}
+	peers.addDynamicView('all_peers')
+	.applyFind({ user_id: { $exists: true }, public_key: { $exists: true } });
+	let revocations = db.getCollection('revocations');
+	if (revocations == null) {
+		revocations = db.addCollection('revocations');
+	}
+	// feels a little stoopid to NOT return all the collections here
+	// but it also feekls stoopid to do the same
+	return { db, channels, peers, revocations };
+}
+
 function init (dbFilename, autosave=true) {
 	return new Promise((resolve, reject) => {
 		const db = new loki(dbFilename, {
@@ -11,26 +31,16 @@ function init (dbFilename, autosave=true) {
 				reject(err);
 			}
 			else {
-				let channels = db.getCollection('channels');
-				if (channels == null) {
-					channels = db.addCollection('channels');
-				}
-				let peers = db.getCollection('peers');
-				if (peers == null) {
-					peers = db.addCollection('peers');
-				}
-				peers.addDynamicView('all_peers')
-				.applyFind({ user_id: { $exists: true }, public_key: { $exists: true } });
-				let revocations = db.getCollection('revocations');
-				if (revocations == null) {
-					revocations = db.addCollection('revocations');
-				}
-				// feels a little stoopid to NOT return all the collections here
-				// but it also feekls stoopid to do the same
-				resolve({ db, channels, peers, revocations });
+				resolve(addCollections(db));
 			}
 		});
 	});
+}
+
+async function bruh () {
+	const db = new loki('bruh.db');
+	db.close = () => {}; // just some memes just in clase
+	return addCollections(db);
 }
 
 module.exports = init;
