b0283ea923100122bd7925101a1c12e35def8204
Jon <jarano93@gmail.com>
2021-06-04 18:08:42 -0700

    haha don't gotta make DO_NOT_SHARE myself haha

--- prepare.bat
+++ prepare.bat
@@ -3,9 +3,9 @@ IF %1.==. GOTO No1
 MD releases\STOWAWAY_v%1_linux
 MD releases\STOWAWAY_v%1_macos
 MD releases\STOWAWAY_v%1_win
-MD releases\STOWAWAY_v%1_linux\DO_NOT_SHARE
-MD releases\STOWAWAY_v%1_macos\DO_NOT_SHARE
-MD releases\STOWAWAY_v%1_win\DO_NOT_SHARE
+rem MD releases\STOWAWAY_v%1_linux\DO_NOT_SHARE
+rem MD releases\STOWAWAY_v%1_macos\DO_NOT_SHARE
+rem MD releases\STOWAWAY_v%1_win\DO_NOT_SHARE
 MOVE releases\STOWAWAY-linux releases\STOWAWAY_v%1_linux\STOWAWAY
 MOVE releases\STOWAWAY-macos releases\STOWAWAY_v%1_macos\STOWAWAY
 MOVE releases\STOWAWAY-win.exe releases\STOWAWAY_v%1_win\STOWAWAY.exe
