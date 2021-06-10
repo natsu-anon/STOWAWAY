c831e1a1daf62bc399d8b2574e06795949a02ab4
Jon <jarano93@gmail.com>
2021-06-10 10:45:05 -0700

    delete keys from testing dirs

---
+++ keys.bat
@@ -0,0 +1,7 @@
+@ECHO OFF
+DEL testing\brusef\DO_NOT_SHARE\stowaway.key
+DEL testing\jamie\DO_NOT_SHARE\stowaway.key
+DEL testing\spurdo\DO_NOT_SHARE\stowaway.key
+DEL testing\brusef\stowaway.revoke
+DEL testing\jamie\stowaway.revoke
+DEL testing\spurdo\stowaway.revoke
